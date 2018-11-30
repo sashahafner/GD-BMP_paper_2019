4c309c4da4fa7e44468917c45b0d34caf79e8e11
Jacob <jacobmortensen@live.dk>
2018-11-30 12:41:15 +0100

    Written code moved into gdComp.R

--- functions/gdComp.R
+++ functions/gdComp.R
@@ -1,1 +1,29 @@
 # Function for calculating biogas composition from gas density
+
+# Closed form function for xCH4
+
+# By Jacob R. Mortensen
+
+#Parameters:
+
+gdComp <- function(
+  temp, # Temperature in K
+  pres, # Pressure of biogas in KPa
+  massloss, # Mass loss in reactor
+  vol.hs, # Volume headspace of reactor
+  unit.temp = getOption('unit.temp', 'C')
+  unit.pres = getOption('unit.pres', 'atm')
+  
+) {
+  
+# Check arguments:
+  
+  checkArgClassValue(massloss, c('integer', 'numeric'), expected.range = c(0, Inf))
+  checkArgClassValue(pres, c('integer', 'numeric')
+  checkArgClassValue(temp, c('integer', 'numeric')
+  checkArgClassValue(vol.hs, c('integer', 'numeric')
+  
+# Unit conversion
+
+  pres.pa <- unitConvert(x = pres, unit = unit.pres, to = 'kPa')
+  temp.k <- unitConvert(x = temp, unit = unit.temp, to = 'K')
