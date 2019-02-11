5b2c01685c1b3cb4e86bc446fa60993cb079f760
camillaglismand <camilla_justesen@hotmail.com>
2019-02-11 13:26:10 +0100

    Add ANOVA and Tukey test to try to look at data

--- experiments/thermal hydrolysis/scripts/numeric_comparison.R
+++ experiments/thermal hydrolysis/scripts/numeric_comparison.R
@@ -26,14 +26,38 @@ cbg.all.c$man.grav.diff <- cbg.all.c$mean_man - cbg.all.c$mean_grav
 cbg.all.c$man.vol.diff <- cbg.all.c$mean_man - cbg.all.c$mean_vol
 cbg.all.c$man.gd.diff <- cbg.all.c$mean_man - cbg.all.c$mean_gd
 
-cbg.diff <- subset(cbg.all.c, select = c(descrip_man, man.grav.diff, man.vol.diff, man.gd.diff))
+cbg.diff <- subset(cbg.all.c, select = c(descrip, man.grav.diff, man.vol.diff, man.gd.diff))
 
 
+# -----------------
 
+# Try using ANOVA
 
+mod.cbg <- aov(mean ~ descrip+method, data = cbg.all)
+summary(mod.cbg)
 
-# ----------------
-# ANOVA
-# mod.BMP <- aov(mean ~ descrip*method, data = cbg.all)
-# summary(mod.BMP)
-# mod.BMP
+# Check for assumptions for ANOVA
+par(mfrow=c(1,1))
+plot(mod.cbg,1)
+plot(mod.cbg,2)
+plot(mod.cbg,3)
+plot(mod.cbg,4)
+plot(mod.cbg,5)
+plot(mod.cbg,6)
+dev.off()
+
+hist(cbg.all$mean)
+
+# Not quite a normal distributed data set
+# If we end up using an ANOVA some kind of transformation may be needed. 
+
+# Try making a Tukey test to test if the methods are different from eachother 
+table.Tukey <- TukeyHSD(mod.cbg, "method", conf.level = 0.95)
+
+png("../plots/Tukey.png", height = 1080/2, width = 1940/3)
+
+par(mar = c(3, 9, 0.4, 2.1))
+plot(table.Tukey, las = 1, col = "blue", cex.axis = 1.1, font = 1)
+
+dev.off()
+
