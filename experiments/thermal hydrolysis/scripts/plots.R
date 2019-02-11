d3bfbfd10bffd0c3038f22bebdca6e5afffe9652
camillaglismand <camilla_justesen@hotmail.com>
2019-02-11 11:34:25 +0100

    Add some plots of BMP and biogas vs time for all four methods

--- experiments/thermal hydrolysis/scripts/plots.R
+++ experiments/thermal hydrolysis/scripts/plots.R
@@ -4,37 +4,45 @@
 ggplot(cbg.grav, aes(elapsed.time, cvCH4, colour = grav.subs.type, group = id )) +
   geom_point() +
   geom_line() + 
+  ggtitle("Gravimetric") +
   labs(x = "Elapsed Time [hr]", y = "Cumulative CH4 [mL]", colour = "Substrate")  + 
   theme_bw() + 
   theme(text = element_text(size = 10))
-
+ggsave('../plots/gravimetric_biogas.png')
 
 # Plot of data before corrected for inoculum (vol)
 ggplot(cbg.vol, aes(elapsed.time, cvCH4, colour = vol.subs.type, group = id )) +
   geom_point() +
   geom_line() + 
+  ggtitle("Volumetric") +
   labs(x = "Elapsed Time [hr]", y = "Cumulative CH4 [mL]", colour = "Substrate")  + 
   theme_bw() + 
   theme(text = element_text(size = 10))
+ggsave('../plots/volumetric_biogas.png')
 
 # Plot of data before corrected for inoculum (man)
 ggplot(cbg.man, aes(elapsed.time, cvCH4, colour = man.subs.type, group = id )) +
   geom_point() +
   geom_line() + 
+  ggtitle("Manometric") +
   labs(x = "Elapsed Time [hr]", y = "Cumulative CH4 [mL]", colour = "Substrate")  + 
   theme_bw() + 
   theme(text = element_text(size = 10))
+ggsave('../plots/manometric_biogas.png')
 
-# EVENTUALLY SAVE SOME OF THE IMPORTANT PLOTS BY SAVING COMMAND!
-
-# Plot of data before corrected for inoculum (man)
+# Plot of data before corrected for inoculum (GD)
 ggplot(cbg.gd, aes(elapsed.time, cvCH4, colour = gd.subs.type, group = id )) +
   geom_point() +
   geom_line() + 
+  ggtitle("GD Method") +
   labs(x = "Elapsed Time [hr]", y = "Cumulative CH4 [mL]", colour = "Substrate")  + 
   theme_bw() + 
   theme(text = element_text(size = 10))
+ggsave('../plots/GD_biogas.png')
 
+
+
+# ----------------------
 
 # Plot mean data for each substrate (with ino and substrate correction) 
 
@@ -43,18 +51,17 @@ ggplot(cbg.all, aes(method, mean, colour = descrip)) +
          labs(x = 'Method', y = 'Mean Cumulative CH4 [mL]', colour = 'Description')  + 
          theme_bw() + 
          theme(text = element_text(size = 10))
-
-# Add plot
+ggsave('../plots/method_comparison_BMP.png')
 
 # Gravimetric and volumetric are similar
 
 # Plot with reverse of method/descrip
 ggplot(cbg.all, aes(descrip, mean, colour = method)) +
-  geom_point() + geom_line() +
+  geom_point() + geom_line(aes(group = method)) +
   labs(x = 'Description', y = 'Mean Cumulative CH4 [mL]', colour = 'Method')  +
   theme_bw() +
   theme(text = element_text(size = 10))
-
+ggsave('../plots/method_comparison_BMP_reverse.png')
 
 # Plot mean data for each substrate (with ino and substrate correction) - But change to get the full amount of bottles
 
