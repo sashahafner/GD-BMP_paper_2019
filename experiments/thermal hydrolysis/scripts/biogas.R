2ae802a664c40d4daf056b732af3eaf4cef2a6f3
camillaglismand <camilla_justesen@hotmail.com>
2019-02-14 12:45:39 +0100

    Update biogas.R

--- experiments/thermal hydrolysis/scripts/biogas.R
+++ experiments/thermal hydrolysis/scripts/biogas.R
@@ -40,9 +40,7 @@ cbg.vol <- cumBg(biogas, dat.type = 'vol', comp = comp, temp = 20, pres = 101.32
                  headspace = setup, vol.hs.name = 'vol.hs', headcomp = 'N2',
                  extrap = TRUE) 
 
-
-
-
+# GD method
 biogas <- biogas[order(biogas$id, biogas$elapsed.time), ]
 cbg.gd <- as.data.frame(mutate(group_by(biogas, id), cvol = cumsum(vol), cmassloss = mass.final[1]-mass.final))
 
@@ -53,26 +51,32 @@ cbg.gd$xCH4 <- gdComp(cbg.gd$cmassloss, cbg.gd$cvol, temp = 20, pres = 101.325)
 # volumetric less sensitive to comp.therefore, use this one with GD. Can also try others afterwards. 
 ## But when using dat.type = 'vol' - wont it affect the results which method is used? 
 cbg.gd <- cumBg(cbg.gd, dat.type = 'vol', temp = 20, pres = 101.325,
-                 data.struct = 'longcombo',
-                 id.name = 'id', time.name = 'elapsed.time',
-                 dat.name = 'vol', comp.name = 'xCH4',
-                 headspace = setup, vol.hs.name = 'vol.hs', headcomp = 'N2',
-                 showt0 = FALSE, temp.init = 20,
-                 addt0 = FALSE, extrap = TRUE) 
+                data.struct = 'longcombo',
+                id.name = 'id', time.name = 'elapsed.time',
+                dat.name = 'vol', comp.name = 'xCH4',
+                headspace = setup, vol.hs.name = 'vol.hs', headcomp = 'N2',
+                showt0 = FALSE, temp.init = 20,
+                addt0 = FALSE, extrap = TRUE) 
 
-BMP.gd <- summBg(cbg.gd, setup, id.name = "id", 
-                 time.name = 'elapsed.time', descrip.name = 'descrip', 
-                 inoc.name = "I", inoc.m.name = "m.inoc", norm.name = "m.sub.vs", 
-                 when = when.BMP, extrap = TRUE)
 
+cbg.list <- list(vol = cbg.vol, man = cbg.man, grav = cbg.grav, gd = cbg.gd) 
+BMP <- summBg(cbg.list, setup, id.name = "id",
+              time.name = 'elapsed.time', descrip.name = 'descrip',
+              inoc.name = "I", inoc.m.name = "m.inoc", norm.name = "m.sub.vs",
+              when = when.BMP, extrap = TRUE, set.name = 'method')
+# Note the set.name argument. The code above would give you a new column (1st one) in the output with the name "method". 
+# You will then be able to cut some code from merge.R as well.
+
+
+# The last two methods
 # o for "obs" = one value for each bottle
-BMPo.gd <- summBg(cbg.gd, setup, id.name = "id", 
-                   time.name = 'elapsed.time', descrip.name = 'descrip', 
-                   inoc.name = "I", inoc.m.name = "m.inoc", norm.name = "m.sub.vs", 
-                   when = when.BMP, extrap = TRUE, show.obs = TRUE)
+#BMPo <- ... show.obs = TRUE)
 
 # yld for yield
-yld.gd <- summBg(cbg.gd, setup, id.name = "id", 
-                  time.name = 'elapsed.time', descrip.name = 'descrip', 
-                  inoc.name = "I", inoc.m.name = "m.inoc", norm.name = "m.sub.vs", 
-                  when = 'meas', extrap = TRUE)
+#yld.gd <- ... when = 'meas')
+
+
+
+
+
+
