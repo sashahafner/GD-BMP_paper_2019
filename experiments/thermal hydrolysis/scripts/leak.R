2643a370a1e6d281524bf7aa33103a684c8f9f71
camillaglismand <camilla_justesen@hotmail.com>
2019-02-15 16:02:59 +0100

    change minor things in leakage functino and leak.R for thermal hydrolysis

--- experiments/thermal hydrolysis/scripts/leak.R
+++ experiments/thermal hydrolysis/scripts/leak.R
@@ -1,4 +1,8 @@
 # Calculate leakage from biogas bottles
 
-leaks <- leakBg(biogas, time.name = 'elapsed.time', m.init.name = 'mass.init', m.final.name = 'mass.final')
+#source('functions.R')
+leaks <- leakBg(biogas, 
+                time.name = 'elapsed.time', 
+                m.init.name = 'mass.init', m.final.name = 'mass.final',
+                id.name = 'id')
 
