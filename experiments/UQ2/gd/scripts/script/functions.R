dff5419de85246b7975bca4de2d329db5d2a2d06
camillaglismand <camilla_justesen@hotmail.com>
2019-02-20 12:29:07 +0100

    Add separate directories for gd and grav_man for UQ2

---
+++ experiments/UQ2/gd/scripts/script/functions.R
@@ -0,0 +1,12 @@
+
+# Load functions
+
+# Dfsumm function is used to get an overview
+source('../../../functions/dfsumm.R')
+
+# gd_comp function for calculation by gd method
+source('../../../functions/gdComp.R')
+
+# Leakage function
+source('../../../functions/massLoss.R')
+
