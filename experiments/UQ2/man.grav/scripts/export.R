7f84607c8acbce5065de5fee1b8adfd3cb162aad
camillaglismand <camilla_justesen@hotmail.com>
2019-02-26 15:16:00 +0100

    Add water samples, detection limit and part of leak calc.

--- experiments/UQ2/man.grav/scripts/export.R
+++ experiments/UQ2/man.grav/scripts/export.R
@@ -6,4 +6,5 @@ write.csv(setup, '../output/setup.csv', row.names = FALSE)
 # Biogas data
 write.csv(biogas, '../output/biogas.csv', row.names = FALSE)
 
-# Water bottles - remember these they are in a different data sheet 
+# Water bottles
+write.csv(water, '../output/water.csv', row.names = FALSE)
--- experiments/UQ2/man.grav/scripts/load.R
+++ experiments/UQ2/man.grav/scripts/load.R
@@ -3,4 +3,4 @@
 fp <- '../UQ2_man_grav.xlsx'
 setup <- as.data.frame(read_xlsx(fp, sheet = 1, skip = 1))
 biogas  <- as.data.frame(read_xlsx(fp, sheet = 2, skip = 1))
-
+water  <- as.data.frame(read_xlsx(fp, sheet = 4, skip = 1))
