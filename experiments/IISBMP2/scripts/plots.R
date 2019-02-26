0e80124ff821c72f86c28642a0286906801dcc4a
camillaglismand <camilla_justesen@hotmail.com>
2019-02-26 09:27:09 +0100

    Re-run IlSBMP2

--- experiments/IISBMP2/scripts/plots.R
+++ experiments/IISBMP2/scripts/plots.R
@@ -109,4 +109,5 @@ dev.off()
 #   geom_line() + geom_point()  + geom_hline(yintercept = log10(c(1, 0.5)))
 # ggsave('../plots/rrates.png')
 
-
+ggplot(yld, aes(elapsed.time, mean, colour = descrip))  + geom_point() + labs(x = 'Time (d)', y = 'CH4 yield (mL/g)', colour = 'descrip')
+ggsave('../plots/yield_gd.png')
