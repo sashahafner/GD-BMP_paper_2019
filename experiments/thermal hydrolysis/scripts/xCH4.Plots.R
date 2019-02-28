818e5380b08f3ec2dd8e6a7250a43b0cc69fb780
camillaglismand <camilla_justesen@hotmail.com>
2019-02-28 11:25:09 +0100

    Add some xCH4 plots and run code for thermal hydrolysis

--- experiments/thermal hydrolysis/scripts/xCH4.Plots.R
+++ experiments/thermal hydrolysis/scripts/xCH4.Plots.R
@@ -2,6 +2,7 @@
 
 # Plot GC measured results
 comp1 <- merge(comp, setup[, c('id', 'descrip')], by = 'id')    # Change to comp later on
+comp1$method <- 'GC'
 
 ggplot(comp1, aes(elapsed.time, xCH4, colour = descrip)) + 
   geom_point() + geom_line(aes(group = id)) +
@@ -13,6 +14,7 @@ ggplot(comp1, aes(elapsed.time, xCH4, colour = descrip)) +
 ggsave('../plots/xCH4comp.png')
 # All but inoculum and celulose decreaes in methane composition during time - is it normal? 
 # Is most methane produced in the beginning of the experiments and then "vented-out"? Hence composition decreases. 
+
 
 # man, grav and vol methods  - These should be the same as it is calculated from GC
 # - What has been done in code to keep the xCH4 at the highest level instead of dropping?
@@ -41,45 +43,51 @@ ggsave('../plots/xCH4man.png')
 # ggsave('../plots/xCH4grav.png')
 
 
-cbg.gd.1 <- cbg.gd[cbg.gd$elapsed.time <20, ]
-ggplot(cbg.gd.1, aes(elapsed.time, xCH4, colour = descrip)) + 
-  geom_point() + geom_line(aes(group = id))
+# ----------------
 
-cbg.gd7[cbg.gd7$xCH4 <0, cbg.gd7$elapsed.time]
-cbg.list.gd <- list(
-  #vol = cbg.vol, man = cbg.man, grav = cbg.grav,
-  # gd1 = cbg.gd1, gd2 = cbg.gd2, gd3 = cbg.gd3, gd4 = cbg.gd4, gd5 = cbg.gd5, gd6 = cbg.gd6, 
-  gd7 = cbg.gd7, gd8 = cbg.gd8, gd9 = cbg.gd9, gd10 = cbg.gd10, gd11 = cbg.gd11, gd12 = cbg.gd12) 
-
-
-
+# Plot all gd methods composition in one plot
 cbg.gd.all <- biogas:::rbindf(
   # cbg.vol, cbg.man, cbg.grav, cbg.gd, 
   #cbg.gd1, cbg.gd2, gbg.gd3, cbg.gd4, cbg.gd5, cbg.gd6, 
+  comp1, 
   #cbg.gd7, 
   cbg.gd8, cbg.gd9, 
   #cbg.gd10, 
   cbg.gd11, cbg.gd12)
-ggplot(cbg.gd.all, aes(elapsed.time, xCH4, colour = descrip)) + 
-  geom_point() + geom_line(aes(group = id))
 
+ggplot(cbg.gd.all, aes(elapsed.time, xCH4, colour = descrip)) + 
+  geom_point() + geom_line(aes(group = id)) + 
+  labs(x = 'Time [d]', y = 'CH4 Composition', colour = 'Substrate')  + 
+  ggtitle('Composition Comparison') +
+  theme_bw() + 
+  theme(text = element_text(size = 8)) +
+  facet_grid(method ~. )   # What is the best grid after adding all (or chosen) gd methods
+ggsave('../plots/xCH4.png')
+
+# ---------------
 
 # Negative xCH4
 ggplot(cbg.gd7, aes(elapsed.time, xCH4, colour = descrip)) + 
   geom_point() + geom_line(aes(group = id))
+ggsave('../plots/xCH4gd7.png')
 ggplot(cbg.gd10, aes(elapsed.time, xCH4, colour = descrip)) + 
   geom_point() + geom_line(aes(group = id))
+ggsave('../plots/xCH4gd10.png')
 
 # Straight (final)
 ggplot(cbg.gd9, aes(elapsed.time, xCH4, colour = descrip)) + 
   geom_point() + geom_line(aes(group = id))
+ggsave('../plots/xCH4gd9.png')
 ggplot(cbg.gd12, aes(elapsed.time, xCH4, colour = descrip)) + 
   geom_point() + geom_line(aes(group = id))
+ggsave('../plots/xCH4gd12.png')
 
 ggplot(cbg.gd8, aes(elapsed.time, xCH4, colour = descrip)) + 
   geom_point() + geom_line(aes(group = id))
+ggsave('../plots/xCH4gd8.png')
 ggplot(cbg.gd11, aes(elapsed.time, xCH4, colour = descrip)) + 
   geom_point() + geom_line(aes(group = id))
+ggsave('../plots/xCH4gd11.png')
 
 # It only seems to be the cumulative and final averaging method that makes sense in order to get methane content. 
 # Using the final averaging method only gives one methane content for the full experiment. 
