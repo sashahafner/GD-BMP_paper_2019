0854472c1ab4b8954fdb6d4476cf53c76f7fe427
camillaglismand <camilla_justesen@hotmail.com>
2019-02-28 09:01:32 +0100

    Add rsd calculation for IlSBMP2

--- experiments/IISBMP2/scripts/BMP.R
+++ experiments/IISBMP2/scripts/BMP.R
@@ -25,10 +25,14 @@ yld <- summBg(cbg.list, setup, id.name = "id",
               inoc.name = "Inoculum", inoc.m.name = "m.inoc", norm.name = "m.sub.vs",
               when = 'meas', extrap = TRUE, set.name = 'method')
 
+# Add rsd to data frames
+BMP$rsd <- 100 * BMP$sd/BMP$mean
+yld$rsd <- 100 * yld$sd/yld$mean
+BMP.1p3d$rsd <- 100 * BMP.1p3d$sd/BMP.1p3d$mean
+
 # Add leaklimit column. TRUE == leak is above detection limit
 BMP$leak.l <- BMP$descrip %in% large.leaks$descrip
 BMPo$leak.l <- BMPo$id %in% large.leaks$id
 BMP.1p3d$leak.l <- BMP.1p3d$descrip %in% large.leaks$descrip
 
 # yld$leak.l <- yld$descrip %in% large.leaks$descrip
-
