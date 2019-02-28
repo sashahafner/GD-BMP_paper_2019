16b1d191ddbd6894920a63fed2df718dfe79caf6
camillaglismand <camilla_justesen@hotmail.com>
2019-02-28 09:04:52 +0100

    Add rsd calc to UQ2.gd

--- experiments/UQ2/gd/scripts/merge.R
+++ experiments/UQ2/gd/scripts/merge.R
@@ -4,3 +4,7 @@
 
 # Add substrate type and other needed variables with merge of some columns from setup
 cbg.gd <- merge(cbg.gd, setup[, c('id', 'descrip')], by = 'id')
+
+# Add rsd to data frames
+BMP$rsd <- 100 * BMP$sd/BMP$mean
+yld$rsd <- 100 * yld$sd/yld$mean
