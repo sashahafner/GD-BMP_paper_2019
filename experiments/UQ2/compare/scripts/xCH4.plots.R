fe09b7843130adf15eedf75a6643d92b3bb90b26
camillaglismand <camilla_justesen@hotmail.com>
2019-03-10 20:24:06 +0100

    Update xCH4.plots.R

--- experiments/UQ2/compare/scripts/xCH4.plots.R
+++ experiments/UQ2/compare/scripts/xCH4.plots.R
@@ -1,44 +1,16 @@
 # Make plots for xCH4
 
-# #cbg.gd$method <- 'gd'
-# cbg.gd1$method <- 'gd1'
-# cbg.gd2$method <- 'gd2'
-# cbg.gd3$method <- 'gd3'
-# # cbg.gd4$method <- 'gd4'
-# cbg.gd5$method <- 'gd5'
-# # cbg.gd6$method <- 'gd6'
-# #cbg.gd7$method <- 'gd7'
-# cbg.gd8$method <- 'gd8'
-# # cbg.gd9$method <- 'gd9'
-# # cbg.gd10$method <- 'gd10'
-# # cbg.gd11$method <- 'gd11'
-# # cbg.gd12$method <- 'gd12'
-
-
-# Plot all gd methods composition in one plot
-cbg.gd.all <- biogas:::rbindf(
-  cbg.gd1, cbg.gd2, cbg.gd3, 
-  #cbg.gd4, 
-  cbg.gd5, 
-  #cbg.gd6, cbg.gd7, 
-  cbg.gd8
-  # cbg.gd9, 
-  # cbg.gd10, 
-  # cbg.gd11, cbg.gd12
-  )
-
-ggplot(data = xCH4) + 
-  geom_point(aes(elapsed.time, xCH4, color = id), na.rm = TRUE) + 
-  geom_point(aes(elapsed.time, xCH4_gd1), na.rm = TRUE)
-
-
-ggplot(xCH4, elapsed.time)) 
-  geom_point() + geom_line() + 
+#xCH4 all comparison
+ggplot(xCH4.all, aes(elapsed.time, xCH4, colour = descrip)) + 
+  geom_point() + geom_line(aes(group = id)) + 
   labs(x = 'Time [d]', y = 'CH4 Composition', colour = 'Substrate')  + 
   ggtitle('Composition Comparison') +
   theme_bw() + 
   theme(text = element_text(size = 8)) +
-ggsave('../plots/xCH4.png')
+  facet_grid(method ~ . )   # What is the best grid after adding all (or chosen) gd methods
+ggsave('../plots/xCH4.comparison.png')
+
+
 
 # ---------------
 #grav/man (GC data)
