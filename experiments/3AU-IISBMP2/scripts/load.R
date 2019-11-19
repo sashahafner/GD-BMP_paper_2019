148b45e50be487862726707e7b816788de7e31ee
Sasha D. Hafner <sasha.hafner@gmail.com>
2019-11-19 14:07:35 -0500

    Correct m.sub.vs in AU data, base on average of 3 labs as described in manuscript. Proof correction version.

--- experiments/3AU-IISBMP2/scripts/load.R
+++ experiments/3AU-IISBMP2/scripts/load.R
@@ -3,3 +3,9 @@
 ff <- '../data/biogas_and_setup.xlsx'
 setup <- as.data.frame(read_xlsx(ff, sheet = 1, skip = 1))
 biogas  <- as.data.frame(read_xlsx(ff, sheet = 2, skip = 1))
+
+# Add TS/VS to setup and recalculate n.sub.vs based on mean VS conc.
+tsvs <- data.frame(substrate = c('CEL', 'SC', 'SD'), c.vs = c(0.9931, 0.8135, 0.8680))
+
+setup <- merge(setup, tsvs, by = 'substrate', all.x = TRUE)
+setup$m.sub.vs <- setup$m.sub * setup$c.vs
